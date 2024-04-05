be07d90c6744e82139bc5cd499ae534e839f64fb
tanujyd22 <tanujydharamshi.22@gmail.com>
2024-04-05 13:37:51 +0530

    initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# flutter_1
-
-A new Flutter project.
--- lib/boxes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hive/hive.dart';
-import 'package:flutter_1/todo.dart';
-
-class Boxes{
-  static Box<Todo> getBox() => Hive.box<Todo>("myBox");
-}
